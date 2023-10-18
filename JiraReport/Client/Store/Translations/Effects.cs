13e960e7690a685fbed5a540061efc647176efa1
Ondřej Kačmár <Ondrej.Kacmar@alza.cz>
2023-10-18 21:16:10 +0200

    nuget update

--- JiraReport/Client/Store/Translations/Effects.cs
+++ JiraReport/Client/Store/Translations/Effects.cs
@@ -6,28 +6,28 @@ using System.Net.Http.Json;
 
 namespace JiraReport.Client.Store.Translations
 {
-    public class Effects : IExceptionStateLogger
-    {
-        private readonly HttpClient _httpClient;
-        private readonly IDispatcher _dispatcher;
+	public class Effects : IExceptionStateLogger
+	{
+		private readonly HttpClient _httpClient;
+		private readonly IDispatcher _dispatcher;
 
-        public Effects(HttpClient httpClient, IDispatcher dispatcher)
-        {
-            _httpClient = httpClient;
-            _dispatcher = dispatcher;
-        }
+		public Effects(HttpClient httpClient, IDispatcher dispatcher)
+		{
+			_httpClient = httpClient;
+			_dispatcher = dispatcher;
+		}
 
-        [EffectMethod]
-        [ExceptionStateBind]
-        public async Task HandleFetchDataAction(FetchTranslationsAction _, IDispatcher dispatcher)
-        {
-            var issueCollection = await _httpClient.GetFromJsonAsync<ReportTranslationCollection>("api/jiraissues/translations");
-            dispatcher.Dispatch(new FetchTranslationsResultAction(issueCollection));
-        }
+		[EffectMethod]
+		[ExceptionStateBind]
+		public async Task HandleFetchDataAction(FetchTranslationsAction _, IDispatcher dispatcher)
+		{
+			var issueCollection = await _httpClient.GetFromJsonAsync<ReportTranslationCollection>("api/jiraissues/translations");
+			dispatcher.Dispatch(new FetchTranslationsResultAction(issueCollection));
+		}
 
-        public void Log(Exception exception)
-        {
-            _dispatcher.Dispatch(new AddExceptionAction(exception));
-        }
-    }
+		public void Log(Exception exception)
+		{
+			_dispatcher.Dispatch(new AddExceptionAction(exception));
+		}
+	}
 }
